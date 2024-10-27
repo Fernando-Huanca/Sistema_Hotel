93a208f54eedb23487711dc94ef3bfaa3159ceac
Freddy Fernando Huanca Irahola <fernandohuanca2005@gmail.com>
2024-10-27 14:23:52 -0400

    Configuraciones iniciales y urls

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+define('ADMIN', 'admin');
+define('RUTA_PRINCIPAL', 'http://localhost:8080/reservas/');
+define('RUTA_ADMIN', RUTA_PRINCIPAL . ADMIN . '/');
+
+?>
---
+++ controllers/principal/Blog.php
@@ -0,0 +1,11 @@
+<?php 
+class Blog{
+   public function index(){
+    echo 'menaje dede el controlador';
+   } 
+
+   public function listar(){
+    echo 'AQUI VAMOS A LISTAR LOS RESULTADOS DE LA BdD';
+   } 
+}
+?>
